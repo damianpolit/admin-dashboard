bee663f05d70759750ea8982fa68790e0f0260eb
Damian Polit <damianpolit@interia.pl>
2018-07-06 09:24:54 +0200

    1.0.0
    nice admin dashboard

--- assets/js/mainController.js
+++
@@ -1,3 +0,0 @@
-app.controller('MainController', ['$scope', function($scope) { 
-  $scope.unact = '<bold>(Nieaktywny)</bold>'; 
-}]);
---
+++ include/menu-include.php
@@ -0,0 +1,8 @@
+<ul class="sidebar-menu">
+          <li class="active">
+            <a class="" href="index.html">
+                          <i class="icon_house_alt"></i>
+                          <span>Panel g³ówny</span>
+                      </a>
+          </li>
+        </ul>
